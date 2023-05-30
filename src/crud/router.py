1860bd7492c1ed3d4b27459ba78db6efca37dd1a
Zin <svetloewill@mail.ru>
2023-05-30 15:21:12 +0300

    2

---
+++ src/crud/router.py
@@ -0,0 +1,70 @@
+import uuid
+from datetime import datetime
+from typing import List
+
+from fastapi import FastAPI, APIRouter, Depends, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from sqlalchemy import update, delete
+from src.crud.models import User
+from src.crud.schemas import CreateUser
+from src.database import get_async_session
+
+router = APIRouter(
+    prefix="/api",
+    tags=["Create and Delete"]
+)
+
+
+@router.get('/get_item_all', response_model=List[CreateUser], status_code=status.HTTP_200_OK)
+async def get_user_all(session: AsyncSession = Depends(get_async_session)):
+    query = select(User)
+    result = await session.execute(query)
+    return [CreateUser(id=i.id, name=i.name, token=i.token) for i in result.scalars().all()]
+
+
+@router.get('/get_item', response_model=List[CreateUser], status_code=status.HTTP_200_OK)
+async def get_user(name: str, session: AsyncSession = Depends(get_async_session)):
+    try:
+        query = select(User).where(User.name == name)
+        result = await session.execute(query)
+        return [CreateUser(id=i.id, name=i.name, token=i.token) for i in result.scalars().all()]
+
+    except Exception:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+        })
+
+
+@router.put('/update')
+async def update_user(name: str, id: int, session: AsyncSession = Depends(get_async_session)):
+    stmt = update(User).where(User.id == id) \
+        .values(name=name, token=uuid.uuid4())
+    await session.execute(stmt)
+    await session.commit()
+
+
+@router.delete('/delete')
+async def delete_user(name: str, id: int, session: AsyncSession = Depends(get_async_session)):
+    stmt = delete(User).where(User.id == id, User.name == name)
+    await session.execute(stmt)
+    await session.commit()
+
+
+@router.post('/create', status_code=status.HTTP_201_CREATED)
+async def create_user(name: str, session: AsyncSession = Depends(get_async_session)):
+    stmt = select(User).where(User.name == name)
+    result = await session.execute(stmt)
+    if result.scalars().first() is None:
+        stmt = User(name=name)
+        session.add(stmt)
+        await session.commit()
+    else:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": datetime.strftime(datetime.utcnow(), "%Y-%m-%dT%H:%M:%S.%fZ"),
+            "details": 'Такое пользователь уже существует'
+        })
