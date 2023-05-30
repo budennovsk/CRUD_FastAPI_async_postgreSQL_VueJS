ff30cf9026c5e2d4a0102585280bad2c81493ea3
Zin <svetloewill@mail.ru>
2023-05-30 14:07:10 +0300

    Initial commit

---
+++ src/crud/models.py
@@ -0,0 +1,19 @@
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+# создаем модель, объекты которой будут храниться в бд
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False, unique=True)
+    token = Column(UUID(as_uuid=True), default=uuid.uuid4)
+
+    def __repr__(self):
+        return f'{self.id!r},{self.name!r},{self.token!r}'
+
---
+++ src/crud/schemas.py
@@ -0,0 +1,13 @@
+import uuid
+
+from pydantic import BaseModel
+
+
+class CreateUser(BaseModel):
+    """Валидация данных"""
+    token: uuid.UUID
+    name: str
+    id: int
+
+    class Config:
+        orm_mode = True
